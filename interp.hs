709edd9706e6f8ddfbc236cba3fe6036efffc6f4
Luke Palmer <lrpalmer@gmail.com>
2010-02-16 06:00:42 -0700

    Modified interp to work with the new HOAS.

--- interp.hs
+++ interp.hs
@@ -1,4 +1,5 @@
-import Vatican
+import qualified Vatican
+import HOAS
 
 data Value
     = VPlus
@@ -6,13 +7,17 @@ data Value
     | VInt Integer
     deriving Show
 
-instance Primitive Value where
+instance Vatican.Primitive Value where
     apply VPlus (VInt x) = VAdd x
     apply (VAdd x) (VInt y) = VInt (x+y)
     apply x y = error $ "Type error when applying (" ++ show x ++ ") to (" ++ show y ++ ")"
 
+program :: (Term t) => t
 program = 
     let_ (fun (\f -> fun (\x -> x))) $ \zero ->
     let_ (fun (\n -> fun (\f -> fun (\x -> f % (n % f % x))))) $ \succ ->
-    let_ (fun (\n -> n % prim (VAdd 1) % prim (VInt 0))) $ \toPrim ->
-    toPrim % (succ % (succ % (succ % zero)))
+    succ % (succ % (succ % zero))
+
+go = 
+    let_ (fun (\n -> n % Vatican.prim (VAdd 1) % Vatican.prim (VInt 0))) $ \toPrim ->
+    toPrim % program

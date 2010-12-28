99d3a89936fa2f59d4ae0a650cc24abfab687ad3
Luke Palmer <Luke Palmer lrpalmer@gmail.com>
2010-12-28 13:02:27 -0700

    Added a timing measuring script.

---
+++ measure.pl
@@ -0,0 +1,28 @@
+#!/usr/bin/perl
+
+use IO::CaptureOutput qw(capture_exec);
+
+if (@ARGV < 2) {
+    die <<USAGE;
+Usage: measure.pl <interp> <limit>
+    Prints timings of interpreters stacked n high, n varying from 0 to <limit>
+    interp: one of bubs, thyer, ref
+USAGE
+}
+
+if (not -f "interp") {
+    system "ghc --make -O interp";
+}
+
+my ($interp, $limit, $output) = @ARGV;
+
+$output ||= 'VInt 3';
+
+for my $i (0..$limit) {
+    my ($stdout, $stderr) = capture_exec("/usr/bin/time -f %U ./interp $interp $i");
+    chomp $stdout;
+    if ($stdout ne $output) {
+        die "Interpreter failure at level $i: expecting '$output', got '$stdout'";
+    }
+    print $stderr;
+}

f7a574bff498d7a38de823e8eb60d486211f254d
Luke Palmer <Luke Palmer lrpalmer@gmail.com>
2010-12-30 14:25:28 -0700

    Added type parameter to DeBruijn.

--- DeBruijn.hs
+++ DeBruijn.hs
@@ -9,24 +9,26 @@ import Control.Monad.Trans.State
 import qualified Data.Map as Map
 import Control.Applicative
 
-data Exp
-    = ELam Exp
-    | EApp Exp Exp
+data Exp a
+    = ELam (Exp a)
+    | EApp (Exp a) (Exp a)
     | EVar Integer
+    | EPrim a
 
 showExp lp ap (ELam e) = parens lp $ "\\. " ++ showExp False False e
 showExp lp ap (EApp t u) = parens ap $ showExp True False t ++ " " ++ showExp True True u
 showExp lp ap (EVar z) = show z
+showExp lp ap (EPrim a) = "[" ++ show a ++ "]"
 
 parens False x = x
 parens True x = "(" ++ x ++ ")"
 
-instance Show Exp where
+instance (Show a) => Show (Exp a) where
     show = showExp False False
 
-newtype DeBruijn = DeBruijn { rundB :: ReaderT (Map.Map Integer Integer) (State Integer) Exp }
+newtype DeBruijn a = DeBruijn { rundB :: ReaderT (Map.Map Integer Integer) (State Integer) (Exp a) }
 
-instance Term DeBruijn where
+instance Term (DeBruijn a) where
     DeBruijn t % DeBruijn u = DeBruijn $ liftA2 EApp t u
     fun f = DeBruijn $ do
         varid <- lift get
@@ -35,5 +37,5 @@ instance Term DeBruijn where
             fmap ELam . rundB . f . DeBruijn $ do
                 EVar <$> asks (Map.! varid)
 
-getDeBruijn :: DeBruijn -> Exp
+getDeBruijn :: DeBruijn a -> Exp a
 getDeBruijn dB = evalState (runReaderT (rundB dB) Map.empty) 0

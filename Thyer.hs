cb38ab71f83e902297121476d967e3103f029b14
Luke Palmer <Luke Palmer lrpalmer@gmail.com>
2010-12-30 12:11:43 -0700

    Added some comments explaining some of the finer points of the code.

--- Thyer.hs
+++ Thyer.hs
@@ -46,52 +46,66 @@ reduce ref = do
             case nodeData fnode of
                 Lambda body -> do
                     let bind = nodeDepth fnode + 1
-                        shift = nodeDepth node - bind   -- ???
-                        node' = Node Unblocked (nodeDepth fnode) (Subst body bind arg shift)
+
+                        -- shift is the amount by which the depths of f's nodes are expected
+                        -- to change.
+                        shift = nodeDepth node - bind
+
+                        -- This is (nodeDepth fnode) instead of (nodeDepth node) in Thyer's
+                        -- paper. It is somewhat irrelevant since we only check depths when we
+                        -- are substituting through a node, and we never subsitute through a 
+                        -- subst node, but I believe this makes more sense.
+                        node' = Node Unblocked (nodeDepth node) (Subst body bind arg shift)
                     Ref.write ref node'
                     reduce ref
                 Prim p -> do
                     argnode <- reduce arg
                     case nodeData argnode of
-                        Prim p'   -> do
-                            let red = Node Blocked 0 (Prim (p `HOAS.apply` p'))
-                            Ref.write ref red
-                            return red
+                        Prim p'   -> sideEffect (Ref.write ref) (Node Blocked 0 (Prim $ p `HOAS.apply` p'))
                         Apply {}  -> blocked
                         Var {}    -> blocked
                         Lambda {} -> fail "Can't apply primitive to lambda"
                         Subst {}  -> fail "Bug - reduced expression ended up a subst"
                 _ -> blocked
         Subst body var arg shift -> do
+            -- This is the code that has the specializing effect.  We *reduce*
+            -- the body, including application nodes, before substituting into it.  
+            -- A simple lazy evaluator would just push down the substitution through
+            -- any type of node, including applications.  cf. Thyer p. 122.
             reduce body
-            substituted <- subst body var arg shift
-            Ref.link ref substituted
-            reduce ref  -- huh...
+            Ref.link ref =<< subst body var arg shift
+            reduce ref
         _ -> blocked
     where
     blocked = do
         node <- Ref.read ref
-        let !node' = node { nodeBlocked = Blocked }
-        Ref.write ref node'
-        return node'
+        sideEffect (Ref.write ref) $! node { nodeBlocked = Blocked }
 
--- subst returns body with the variable at depth bind substituted for arg.
+sideEffect :: (a -> IO ()) -> a -> IO a
+sideEffect f x = f x >> return x
+
+-- subst returns body with the variable at depth bind substituted for arg.  It
+-- does not modify its arguments.
 subst :: NodeRef a -> Int -> NodeRef a -> Int -> IO (NodeRef a)
 subst body bind arg shift = do  
     bodynode <- Ref.read body
-    let newdepth = nodeDepth bodynode + shift
+    -- if the depth of the body is less than the depth of the variable we are 
+    -- substituting, the variable cannot possibly occur in the body, so just 
+    -- dissolve away.
     if nodeDepth bodynode < bind then return body else do
+    
+    let newdepth = nodeDepth bodynode + shift
     case nodeData bodynode of
-                Var | nodeDepth bodynode == bind -> return arg
-                    | otherwise                  -> Ref.new (Node Blocked newdepth Var)
-                Lambda body -> do
-                    substbody <- Ref.new (Node Unblocked (newdepth+1) (Subst body bind arg shift))
-                    Ref.new (Node Unblocked newdepth (Lambda substbody))
-                Apply f x -> do
-                    f' <- Ref.new (Node Unblocked newdepth (Subst f bind arg shift)) 
-                    x' <- Ref.new (Node Unblocked newdepth (Subst x bind arg shift))
-                    Ref.new (Node Unblocked newdepth (Apply f' x'))
-                _ -> return body
+        Var | nodeDepth bodynode == bind -> return arg
+            | otherwise                  -> Ref.new (Node Blocked newdepth Var)
+        Lambda body -> do
+            substbody <- Ref.new (Node Unblocked (newdepth+1) (Subst body bind arg shift))
+            Ref.new (Node Unblocked newdepth (Lambda substbody))
+        Apply f x -> do
+            f' <- Ref.new (Node Unblocked newdepth (Subst f bind arg shift)) 
+            x' <- Ref.new (Node Unblocked newdepth (Subst x bind arg shift))
+            Ref.new (Node Unblocked newdepth (Apply f' x'))
+        _ -> return body
 
 fromDepth :: Depth.ExpNode a -> IO (NodeRef a)
 fromDepth (d, n) = case n of

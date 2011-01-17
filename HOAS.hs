b9109426ff475bc119ca384d1c8ba5d9864fecce
Luke Palmer <Luke Palmer lrpalmer@gmail.com>
2011-01-16 18:26:37 -0700

    Added some scott encodings of products and coproducts.

--- HOAS.hs
+++ HOAS.hs
@@ -1,8 +1,18 @@
 {-# LANGUAGE MultiParamTypeClasses, FunctionalDependencies #-}
 
 -- A simple typeclass for terms in HOAS.
+-- Also some scott encodings.
 
-module HOAS (Primitive(..), Term(..), PrimTerm(..)) where
+module HOAS 
+    ( Primitive(..)
+    , Term(..)
+    , PrimTerm(..)
+    , scottTuple
+    , scottUntuple
+    , scottProj
+    , scottCoprod
+    , scottUncoprod
+    ) where
 
 class (Show a) => Primitive a where
     apply :: a -> a -> a
@@ -19,16 +29,42 @@ class Term t where
     fix = fun (\f -> fun (\x -> x % x) % fun (\x -> f % (x % x)))
     
     letrec :: ([t] -> ([t], t)) -> t
-    letrec defns = scottProj dsd 2 1
+    letrec defns = scottProj 2 1 % dsd
         where
         dsd = fix % fun (\dsd -> 
-            let_ (scottProj dsd 2 0) $ \ds ->
-            let (rs,r) = defns (map (scottProj ds n) [0..n-1])
+            let_ (scottProj 2 0 % dsd) $ \ds ->
+            let (rs,r) = defns (map (\i -> scottProj n i % ds) [0..n-1])
                 n      = length rs
-            in scottTuple [scottTuple rs, r])
-        scottProj xs n i = xs % nestedFun n (!! i)
-        scottTuple xs = fun (\f -> foldl (%) f xs)
-    
+            in listToScottTuple [listToScottTuple rs, r])
+
+
+-- scottTuple 4 = \a b c d -> \elim -> elim a b c d
+scottTuple :: (Term t) => Int -> t
+scottTuple n = nestedFun n (\xs -> fun (\elim -> nestedApp elim xs))
+
+-- scottUntuple 4 p = \t -> t (\a b c d -> p [a,b,c,d])
+scottUntuple :: (Term t) => Int -> ([t] -> t) -> t
+scottUntuple n f = fun (\t -> t % nestedFun n f)
+
+-- scottProj 4 1 = \f -> f (\a b c d -> b)
+scottProj :: (Term t) => Int -> Int -> t
+scottProj n i = scottUntuple n (!! i)
+
+listToScottTuple :: (Term t) => [t] -> t
+listToScottTuple xs = nestedApp (scottTuple (length xs)) xs
+
+-- scottCoprod 4 1 = \x -> \a b c d -> b x
+scottCoprod :: (Term t) => Int -> Int -> t
+scottCoprod n i = fun (\x -> nestedFun n (\ps -> (ps !! i) % x))
+
+-- scottUncoprod [f,g,h,i] = \p -> p f g h i
+scottUncoprod :: (Term t) => [t -> t] -> t
+scottUncoprod fs = fun (\p -> nestedApp p (map fun fs))
+
+
+nestedApp :: (Term t) => t -> [t] -> t
+nestedApp = foldl (%)
+
 nestedFun :: (Term t) => Int -> ([t] -> t) -> t
 nestedFun 0 f = f []
 nestedFun n f = fun (\x -> nestedFun (n-1) (f . (x:)))

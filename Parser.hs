5c10e82725361cc6957878aaa2354875c9e32b31
Luke Palmer <luqui@clozure.com>
2012-11-18 22:07:44 -0700

    Added quotation to the parser.

--- Parser.hs
+++ Parser.hs
@@ -6,6 +6,7 @@ import qualified Text.Parsec.Token as P
 import qualified Data.Char as Char
 import qualified DeBruijn as DB
 import qualified Data.Map as Map
+import qualified Data.Set as Set
 import Control.Applicative
 import Control.Monad.Trans.Reader
 import Data.Traversable (sequenceA)
@@ -31,7 +32,7 @@ lex = P.makeTokenParser P.LanguageDef {
     P.identLetter     = preds [ Char.isAlphaNum, (`elem` "_-'") ],
     P.opStart         = opchar,
     P.opLetter        = opchar,
-    P.reservedNames   = [],
+    P.reservedNames   = [ "quote" ],
     P.reservedOpNames = [ "\\", "->" ],
     P.caseSensitive   = True
 }
@@ -48,19 +49,45 @@ ident = P.identifier lex <|> P.parens lex (P.operator lex)
 exp :: Parser Exp
 exp = foldl1 App <$> P.many1 term
     where
-    term = var <|> lambda <|> parenExp
+    term = quotation <|> var <|> lambda <|> parenExp
+    quotation = quote <$> (P.reserved lex "quote" *> term)
     var = Var <$> P.identifier lex
     lambda = flip (foldr Lambda)
            <$> (P.reservedOp lex "\\" *> P.many1 ident) 
            <*> (P.reservedOp lex "->" *> opExp)
     parenExp = P.parens lex opExp
 
+usedVars :: Exp -> Set.Set String
+usedVars (Lambda s e) = Set.delete s (usedVars e)
+usedVars (App f x) = usedVars f `Set.union` usedVars x
+usedVars (Var x) = Set.singleton x
+
+makeFresh :: Set.Set String -> String -> String
+makeFresh set x0 = head [ x0 ++ n | n <- "":map show [0..], not (x0 `Set.member` set) ] 
+
+quote :: Exp -> Exp
+quote e = Lambda lam $ Lambda app $ go Set.empty e
+    where
+    used = usedVars e
+    lam = makeFresh used "lam"
+    app = makeFresh used "app"
+    inj = makeFresh used "inj"
+
+    go seen (Lambda v body) = App (Var lam) (Lambda v (go (Set.insert v seen) body))
+    go seen (App f x)       = App (App (Var app) (go seen f)) (go seen x)
+    go seen (Var v)
+        | v `Set.member` seen = Var v
+        | otherwise           = App (Var inj) (Var v)
+
 toDeBruijn :: Exp -> DB.Exp a
 toDeBruijn = flip runReader Map.empty . go
     where
     go (Lambda v body) = DB.ELam <$> local (Map.insert v 0 . Map.map succ) (go body)
     go (App t u) = liftA2 DB.EApp (go t) (go u)
-    go (Var v) = DB.EVar <$> asks (Map.! v)
+    go (Var v) = DB.EVar <$> get v
+
+    get v = asks (maybe (error ("Unbound variable: " ++ v)) id . Map.lookup v)
+        
 
 parse :: String -> Either P.ParseError (DB.Exp a)
 parse = fmap toDeBruijn . P.parse opExp "<input>"
